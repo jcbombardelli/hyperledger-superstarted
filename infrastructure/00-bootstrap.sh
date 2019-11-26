f083ab862437cc356c02f646e8a385a2dc332b27
JC Bombardelli <jc.bombardelli@live.com>
2019-11-26 09:21:21 -0300

    #3 Update Version Hyperldger Fabric to 1.4.4

--- infrastructure/00-bootstrap.sh
+++ infrastructure/00-bootstrap.sh
@@ -14,7 +14,16 @@ rm -rf ../crypto-config/*
 
 mkdir -p ./.hfc-key-store
 
-curl -sSL http://bit.ly/2ysbOFE | bash -s 1.3.0
+# for use version 1.3.0, uncomment this line above and comment line 21 
+# curl -sSL http://bit.ly/2ysbOFE | bash -s 1.3.0
+
+# curl -sSL http://bit.ly/2ysbOFE | bash -s -- <fabric_version> <fabric-ca_version> <thirdparty_version>
+curl -sSL http://bit.ly/2ysbOFE | bash -s -- 1.4.4 1.4.4 0.4.18
 
 cp -r ./fabric-samples/bin ../
 mv ./fabric-samples ../../
+
+cd ..
+echo export HYPERLEDGER=$PWD/bin >> ~/.bashrc
+source ~/.bashrc
+export PATH=$HYPERLEDGER:$PATH
